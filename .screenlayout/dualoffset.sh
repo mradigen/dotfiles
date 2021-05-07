028468beeb0f07524805634ab00c8f572451afb7
MrADIGEN <adigen@Cysis.localdomain>
2021-05-07 21:32:31 +0400

    Learning/Testing git

---
+++ .screenlayout/dualoffset.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --primary --mode 1366x768 --pos 0x180 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output VGA1 --mode 1280x1024 --pos 1491x0 --rotate normal --output VIRTUAL1 --off
