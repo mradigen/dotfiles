98585700ee17fc007227a452c6c2527da01c0101
mradigen <55953083+mradigen@users.noreply.github.com>
2021-12-22 00:57:18 +0530

    Script rewrite

--- Documents/Extra/install.sh
+++ Documents/Extra/install.sh
@@ -1,35 +1,40 @@
 #! /bin/sh
 
+# You preferably wanna run `sudo su` before this script to be able to execute everything without re-entering root password.
+
+# Get closest mirrors before installing stuff
+sudo pacman -Syy
+sudo pacman -S reflector
+sudo reflector --verbose --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
+
 # Update
-sudo pacman -Syu
+sudo pacman -Syyuu
+sudo pacman -S --needed git base-devel wget
 
-# Drivers and Softwares
-sudo pacman -S xorg xorg-xinit mesa xf86-video-intel xf86-video-vesa libva libva-intel-driver libva-mesa-driver wpa_supplicant connman linux-headers broadcom-wl-dkms
+git clone --recurse-submodules https://github.com/mradigen/dotfiles.git # Recurse to install submodules like goyo and todo-startpage
 
-# Externals
+cd dotfiles
+
+wget https://raw.githubusercontent.com/mradigen/dotfiles/main/Documents/Extra/packages
+
+# yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+# mv yay ~/.local/share/Trash/files
+rm -r yay
 
 # Oh my Zsh
 sudo pacman -S zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# yay (AUR)
-sudo pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd ..
-mv yay ~/.local/share/Trash/files
-
-# Side dependencies
-sudo pacman -S fuse ueberzug unzip youtube-dl rsync
-
-# Programs
-sudo pacman -S bspwm sxhkd ranger alacritty dunst htop neovim rofi rofi-calc vivaldi vivaldi-ffmpeg-codecs feh python-pywal
-
-yay -S flameshot rofigen-git rofi-greenclip polybar sublime-text-4 picom-ibhagwan-git connman-gtk discord_arch_electron apulse themix-theme-oomox-git
+# Install EVERYTHING
+yay -S --needed - < packages
+# yay -S xorg xorg-xinit mesa xf86-video-intel xf86-video-vesa libva libva-intel-driver libva-mesa-driver wpa_supplicant connman linux-headers broadcom-wl-dkms fuse ueberzug unzip youtube-dl rsync bspwm sxhkd ranger moc alacritty dunst htop neovim rofi rofi-calc qutebrowser python-adblock feh python-pywal flameshot rofigen-git rofi-greenclip polybar sublime-text-4 picom-ibhagwan-git connman-gtk discord_arch_electron apulse themix-theme-oomox-git
 
 # Copy dotfiles
-rsync rsync --exclude ".git/" . ~
+rsync -r --exclude ".git/" . ~
 
 # Change execution permissions
 chmod +x ~/.config/alacritty/fixtheme.sh
