742c349e202fef1f499d046b73bc90b4d6537b78
mradigen <55953083+mradigen@users.noreply.github.com>
2023-01-11 18:49:59 +0530

    updots

--- .config/polybar/music.sh
+++ .config/polybar/music.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-playerctl metadata -f "%{F$(xrdb -query | grep '*color7'| awk '{print $NF}')}{{duration(mpris:length - position)}}%{F-}" 2> /dev/null || echo "%{F$(xrdb -query | grep '*color5'| awk '{print $NF}')}--:--%{F-}"
+playerctl --player=youtube-music metadata -f "%{F$(xrdb -query | grep '*color7'| awk '{print $NF}')}{{duration(mpris:length - position)}}%{F-}" 2> /dev/null || echo "%{F$(xrdb -query | grep '*color5'| awk '{print $NF}')}--:--%{F-}"
 
 # Switched to youtube-music
 #musicplay=`mocp -Q %tl 2> /dev/null`
