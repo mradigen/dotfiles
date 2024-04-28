5276b3405610f7a86ca606da96f508e588525da4
mradigen <55953083+mradigen@users.noreply.github.com>
2024-04-28 10:33:35 +0530

    updots

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -166,6 +166,16 @@ local plugins = {
 			},
 			autotag = {
 				enable = true,
+				enable_rename = true,
+				enable_close = true,
+				enable_close_on_slash = true,
+				filetypes = {
+					'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'tsx', 'jsx', 'rescript',
+					'xml',
+					'php',
+					'markdown',
+					'astro', 'glimmer', 'handlebars', 'hbs'
+				}
 			},
 		},
 	},
